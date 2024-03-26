1042a08b42416b18f9d6f61820c0e6c1ee5dadd3
glasdou <dr.glasdou@gmail.com>
2024-03-25 21:16:15 -0600

    Update database URL and add NATS servers configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,14 +4,19 @@ import * as Joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
 }
 
 const envSchema = Joi.object({
   PORT: Joi.number().required(),
   DATABASE_URL: Joi.string().required(),
+  NATS_SERVERS: Joi.array().items(Joi.string()).required(),
 }).unknown(true);
 
-const { error, value } = envSchema.validate(process.env);
+const { error, value } = envSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) throw new Error(`Config validation error: ${error.message}`);
 
@@ -20,4 +25,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
+  natsServers: envVars.NATS_SERVERS,
 };
